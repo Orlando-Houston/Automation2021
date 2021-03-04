ec419b047d01ae43a661187ff7267b673783d191
Altun <a.ozder@outlook.com>
2021-03-04 08:37:07 -0500

    TNGTest

---
+++ src/Jun_4/TNGTest.java
@@ -0,0 +1,20 @@
+package Jun_4;
+
+import org.junit.jupiter.api.Test;
+
+public class TNGTest {
+    @Test
+    public void test1() {
+        System.out.println("@Test1 method");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("@Test2 method");
+    }
+
+    @Test
+    public void test3() {
+        System.out.println("@Test3 method");
+    }
+}
